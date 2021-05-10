9426e01de2f8d7213fee1eacb23e7a6645f17894
eelimss <79057653+eelimss@users.noreply.github.com>
2021-05-10 14:48:54 +0300

    Add files via upload

---
+++ scripts/lamp.js
@@ -0,0 +1,8 @@
+
+lamp.onmouseover = function() {
+    document.body.style.backgroundImage = 'url(./img/main-bg-2.jpg)';
+  };
+  
+  lamp.onmouseout = function() {
+    document.body.style.backgroundImage = 'url(./img/main-bg.jpg)';
+  };
